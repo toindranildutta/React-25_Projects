6c63e466e45a6ae032948dfe6010ab2d188bb383
Indranil_Dutta <toindranildutta@gmail.com>
2024-04-20 08:22:54 +0530

    Random Color Generator added

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,15 @@ import App from './App.jsx'
 import './index.css'
 import GithubProfileFinder from './components/github-profile-finder/index.jsx'
 import Accordian from './components/accordian/index.jsx'
+import RandomColor from './components/random-color/RandomColor.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Accordian />
+    {/* <Accordian /> */}
+    
+    {/* Random Color Component */}
+    <RandomColor />
+
     {/* Github Profile Finder */}
     {/* <GithubProfileFinder /> */}
   </React.StrictMode>,
